c1dca7baabecca3809b9d9842a539a9096c71043
XiaoliChan <2209553467@qq.com>
2023-04-17 12:36:48 +0800

    [Enhancement] Improve old system command execution in silent mode.
    Signed-off-by: XiaoliChan <2209553467@qq.com>

--- lib/modules/exec_command.py
+++ lib/modules/exec_command.py
@@ -3,6 +3,7 @@ import time
 import base64
 import os
 import time
+import datetime
 
 from lib.methods.classMethodEx import class_MethodEx
 from lib.methods.executeVBS import executeVBS_Toolkit
@@ -24,7 +25,7 @@ class EXEC_COMMAND():
 
     # For system under NT6, like windows server 2003
     # Timer for countdown Win32_ScheduledJob, scheduled task in "Win32_ScheduledJob" only will be trigger every per minute.
-    def timer_ForNT6(self, iWbemServices=None, return_iWbemServices=False):
+    def timer_For_UnderNT6(self, iWbemServices=None, return_iWbemServices=False):
         if iWbemServices is None:
             iWbemServices = self.iWbemLevel1Login.NTLMLogin('//./root/Cimv2', NULL, NULL)
             self.iWbemLevel1Login.RemRelease()
@@ -35,24 +36,57 @@ class EXEC_COMMAND():
 
         # Get remaining seconds until the next minute.
         for i in range((65-int(LocalTime['Second']['value'])),0,-1):
-            print(f"[+] Waiting {i}s for next step.", end="\r", flush=True)
+            print(f"[+] Waiting {i}s for command execution.", end="\r", flush=True)
             time.sleep(1)
         
+        print("\r\n[+] Command executed!")
+
         iWbemServices.RemRelease()
         # Return cimv2
         if return_iWbemServices is True: return iWbemServices
+    
+    # For system under NT6, like windows server 2003
+    def exec_command_silent_For_UnderNT6(self, command=None):
+        iWbemServices = self.iWbemLevel1Login.NTLMLogin('//./root/Cimv2', NULL, NULL)
+        self.iWbemLevel1Login.RemRelease()
+        
+        sql1 = "SELECT * FROM Win32_LocalTime"
+        sql2 = "SELECT * FROM Win32_TimeZone"
+        
+        iEnumWbemClassObject = iWbemServices.ExecQuery(sql1)
+        Win32_LocalTime = iEnumWbemClassObject.Next(0xffffffff, 1)[0]
+
+        Machine_Date = datetime.datetime(100, 1, 1, int(Win32_LocalTime.Hour), int(Win32_LocalTime.Minute), 00)
+        execute_time = Machine_Date + datetime.timedelta(0, 60) # Delay one miniute for command execution
+        execute_time=execute_time.time()
+
+        iEnumWbemClassObject = iWbemServices.ExecQuery(sql2)
+        Win32_TimeZone = iEnumWbemClassObject.Next(0xffffffff, 1)[0]
+
+        executeTime = "********" + str(execute_time).replace(":", '') + ".000000+" + str(Win32_TimeZone.Bias)
+        command=r'C:\Windows\System32\cmd.exe /c %s'%command
+        Win32_ScheduledJob,resp=iWbemServices.GetObject("Win32_ScheduledJob")
+        result = Win32_ScheduledJob.Create(command, executeTime, 0, 0, 0, 1)
+        if int(result.ReturnValue) == 0:
+            print("[+] Create schedule job for command execution successfully.")
+        else:
+            print("[-] Create schedule job for command execution error, code: %s" %str(result.ReturnValue))
+
+        self.timer_For_UnderNT6(iWbemServices)
+
+        iWbemServices.RemRelease()
 
     def exec_command_silent(self, command, old=False):
-        executer = executeVBS_Toolkit(self.iWbemLevel1Login)
-
-        random_TaskName = str(uuid.uuid4())
-
-        if '"' in command: command = command.replace('"',r'""')
-        if "'" in command: command = command.replace("'",r'""')
-        
-        print("[+] Executing command...(Sometime it will take a long time, please wait)")
-
         if old == False:
+            executer = executeVBS_Toolkit(self.iWbemLevel1Login)
+
+            random_TaskName = str(uuid.uuid4())
+
+            if '"' in command: command = command.replace('"',r'""')
+            if "'" in command: command = command.replace("'",r'""')
+            
+            print("[+] Executing command...(Sometime it will take a long time, please wait)")
+
             with open('./lib/vbscripts/Exec-Command-Silent.vbs') as f: vbs = f.read()
             vbs = vbs.replace('REPLACE_WITH_COMMAND',command).replace('REPLACE_WITH_TASK',random_TaskName)
             
@@ -65,16 +99,12 @@ class EXEC_COMMAND():
             for i in range(5,0,-1):
                 print(f"[+] Waiting {i}s for next step.", end="\r", flush=True)
                 time.sleep(1)
+            
+            executer.remove_Event(tag)
         else:
-            with open('./lib/vbscripts/Exec-Command-Silent-UnderNT6.vbs') as f: vbs = f.read()
-            vbs = vbs.replace('REPLACE_WITH_COMMAND',command)
-            
-            tag = executer.ExecuteVBS(vbs_content=vbs, returnTag=True)
-            
-            self.timer_ForNT6()
-        
-        executer.remove_Event(tag)
-    
+            # Windows will auto remove job after schedule job executed.
+            self.exec_command_silent_For_UnderNT6(command)
+
     def exec_command_WithOutput(self, command, CODEC="gbk", ClassName_StoreOutput=None, save_Result=False, hostname=None, old=False):
         executer = executeVBS_Toolkit(self.iWbemLevel1Login)
         if ClassName_StoreOutput == None: ClassName_StoreOutput = "Win32_OSRecoveryConfigurationDataBackup"
@@ -110,7 +140,7 @@ class EXEC_COMMAND():
             tag = executer.ExecuteVBS(vbs_content=vbs, returnTag=True)
             
             # Reuse cimv2
-            iWbemServices_Reuse = self.timer_ForNT6(iWbemServices=iWbemServices_Reuse, return_iWbemServices=True)
+            iWbemServices_Reuse = self.timer_For_UnderNT6(iWbemServices=iWbemServices_Reuse, return_iWbemServices=True)
         
         executer.remove_Event(tag)
 
