44704a2da4c1d4aad6698da71a35e3616ebf850d
Glynx <152297+glynx@users.noreply.github.com>
2024-11-23 13:09:27 +0100

    make script references relative to main file

--- lib/modules/eventlog_fucker.py
+++ lib/modules/eventlog_fucker.py
@@ -1,5 +1,6 @@
 from lib.methods.executeVBS import executeVBS_Toolkit
 from impacket.dcerpc.v5.dtypes import NULL
+from lib.helpers import get_vbs_path
 
 class eventlog_Toolkit():
     def __init__(self, iWbemLevel1Login):
@@ -7,7 +8,7 @@ class eventlog_Toolkit():
 
     def fuck_EventLog(self):
         executer = executeVBS_Toolkit(self.iWbemLevel1Login)
-        tag = executer.ExecuteVBS(vbs_file='lib/vbscripts/ClearEventlog.vbs', returnTag=True)
+        tag = executer.ExecuteVBS(vbs_file=get_vbs_path('ClearEventlog.vbs'), returnTag=True)
         print("[+] Keepping note of this tag if you want to stop it: %s"%tag)
     
     def retrieve_EventLog(self, tag):
